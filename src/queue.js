c58f6d605b94cc8dec4547f61127f9ec495a909a
Olga Serova <lyolka92@gmail.com>
2020-05-21 18:48:47 +0300

    fix: add forgotten semicolons

--- src/queue.js
+++ src/queue.js
@@ -1,34 +1,34 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize = 30) {
-		this.maxSize = maxSize;
-		this.heap = new MaxHeap;
-	}
+    constructor(maxSize = 30) {
+        this.maxSize = maxSize;
+        this.heap = new MaxHeap;
+    }
 
-	push(data, priority) {
-		if (this.size() === this.maxSize) {
-			throw new Error("The queue is full. Node isn't added.")
-		} else {
-			this.heap.push(data, priority);
-		}
-	}
+    push(data, priority) {
+        if (this.size() === this.maxSize) {
+            throw new Error("The queue is full. Node isn't added.");
+        } else {
+            this.heap.push(data, priority);
+        }
+    }
 
-	shift() {
-		if (this.isEmpty()) {
-			throw new Error("There is no nodes to shift - queue is empty.")
-		} else {
-			return this.heap.pop();
-		}
-	}
+    shift() {
+        if (this.isEmpty()) {
+            throw new Error("There is no nodes to shift - queue is empty.");
+        } else {
+            return this.heap.pop();
+        }
+    }
 
-	size() {
-		return this.heap.size();
-	}
+    size() {
+        return this.heap.size();
+    }
 
-	isEmpty() {
-		return this.heap.isEmpty();
-	}
+    isEmpty() {
+        return this.heap.isEmpty();
+    }
 }
 
 module.exports = PriorityQueue;
