e23ce1924d344fa7f937dc41bebcfa03601816a9
Olga Serova <lyolka92@gmail.com>
2020-05-21 17:54:57 +0300

    feat: add constructor, push, shift, size and isEmpty to queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,33 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if (this.size() === this.maxSize) {
+			throw new Error("The queue is full. Node isn't added.")
+		} else {
+			this.heap.push(data, priority);
+		}
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error("There is no nodes to shift - queue is empty.")
+		} else {
+			return this.heap.pop();
+		}
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
